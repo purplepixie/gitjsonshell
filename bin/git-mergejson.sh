d9e8d4f651414ec1915e18c8dc1b7605bc341509
David Cutting <d.cutting@qub.ac.uk>
2020-11-08 15:29:25 +0000

    Removed hardcoded names from merge

--- bin/git-mergejson.sh
+++ bin/git-mergejson.sh
@@ -2,5 +2,4 @@
 
 # OPTIONAL: Use jq to merge the two JSON files.
 
-jq --slurp '.[1] as $logstat | .[0] | map(.paths = $logstat[.commit])' git-log.json git-stat.json
-
+jq --slurp '.[1] as $logstat | .[0] | map(.paths = $logstat[.commit])' $1 $2
